8682acbfc1fcf680bc3cb11b20e097ae5c4a6dc5
Kirill Konovalov <kir.kon2018@gmail.com>
2021-10-18 18:52:12 +0300

    2

--- Text_editor/main.py
+++ Text_editor/main.py
@@ -6,19 +6,25 @@ from tkinter.filedialog import asksaveasfile, askopenfile #для открыти
 from tkinter.messagebox import showerror #для отображения ошибок
 from tkinter.font import families
 from tkinter import messagebox
+import tkinter.colorchooser
 
 FILE_NAME = tkinter.NONE
+check_save = False
 
 def new_file():
     global FILE_NAME
     FILE_NAME = "Untitled"
     text.delete('1.0', tkinter.END)
+    global check_save
+    check_save = False
 
 def save_file():
     data = text.get('1.0', tkinter.END)
     out = open(FILE_NAME, 'w')
     out.write(data)
     out.close()
+    global check_save
+    check_save = True
 
 def save_as():
     out = asksaveasfile(mode='w', defaultextension='txt')
@@ -27,6 +33,8 @@ def save_as():
         out.write(data.rstrip())
     except Exception:
         showerror(title="Ошибка", message="Нельзя сохранить")
+    global check_save
+    check_save = True
 
 def open_file():
     global FILE_NAME
@@ -37,13 +45,39 @@ def open_file():
     data = inp.read()
     text.delete('1.0', tkinter.END)
     text.insert('1.0', data)
+    global check_save
+    check_save = False
 
 def on_closing():
+    global check_save
     if tkinter.Text.edit_modified(text) == True:
-        if messagebox.askokcancel("Выход", "Файл не сохранён, продолжить?"):
+        if check_save == False:
+            if messagebox.askokcancel("Выход", "Файл не сохранён, продолжить?"):
+                root.destroy()
+        else:
             root.destroy()
     else:
         root.destroy()
+
+def color():
+        try:
+            (rgb, hx) = tkinter.colorchooser.askcolor()
+            text.tag_add('color', 'sel.first', 'sel.last')
+            text.tag_configure('color', foreground=hx)
+        except TclError:
+            pass
+
+def undo():
+    try:
+        text.edit_undo()
+    except TclError:
+        pass
+
+def redo():
+    try:
+        text.edit_redo()
+    except TclError:
+        pass
 
 root = tkinter.Tk()
 root.title("Текстовый документ")
@@ -51,7 +85,7 @@ root.title("Текстовый документ")
 root.minsize(width=1000, height=600) #размер окна
 root.maxsize(width=1000, height=600)
 
-text = tkinter.Text(root, width=400, height=400, wrap="word", undo=True) #размер текстового поля, перенос по словам, возможность отмены/повтора.
+text = tkinter.Text(root, width=400, height=400, wrap="word", undo=True, font="times") #размер текстового поля, перенос по словам, возможность отмены/повтора.
 scrollb = Scrollbar(root, orient=VERTICAL, command=text.yview)
 scrollb.pack(side="right", fill="y")
 text.configure(yscrollcommand=scrollb.set)
@@ -64,7 +98,13 @@ fileMenu.add_command(label="Открыть", command=open_file)
 fileMenu.add_command(label="Сохранить", command=save_file)
 fileMenu.add_command(label="Сохранить как", command=save_as)
 
+EditMenu = tkinter.Menu(menuBar, tearoff=0)
+EditMenu.add_command(label="Отменить", command=undo, accelerator="Ctrl+Z")
+EditMenu.add_command(label="Повторить", command=redo, accelerator="Ctrl+Y")
+EditMenu.add_command(label="Цвет", command=color)
+
 menuBar.add_cascade(label="Файл", menu=fileMenu)
+menuBar.add_cascade(label="Правка", menu=EditMenu)
 menuBar.add_cascade(label="Выход", command=on_closing)
 root.config(menu=menuBar)
 
